bb9e72a14075e62ce1ea89d4bf0cb274fc6bc7e9
ShyamCMIT <100761502+ShyamCMIT@users.noreply.github.com>
2022-03-14 15:24:21 +0530

    Update Calculator.py

--- Shyam_PGB2022/Python/09-03-2022/14-03-2022/src/Calculator.py
+++ Shyam_PGB2022/Python/09-03-2022/14-03-2022/src/Calculator.py
@@ -4,35 +4,40 @@ class FormulaError(Exception):
     def __init__(self, msg):
         self.msg = msg
 
-def Calculate(l):
-    try:
-        regex = r'(\+|-)'
 
-        if(len(l) != 3):
-            raise FormulaError
+def test(inputx):
+    regex = r'(\+|-)'
+    l = inputx.split()
+    try:
+        if len(l) < 3:
+            raise FormulaError("Less than 3")
+        a, operator, b = l
+        #print(a,b)
 
         try:
-            a = float(l[0])
-            b = float(l[2])
-        except ValueError as e:
+            a = float(a)
+            b = float(b)
+        except ValueError:
+            raise FormulaError("Float to str")
+            #print("hello")
 
-            raise FormulaError("The first and last arguments must be numbers!") from e
-
-
-        if(not re.fullmatch(regex, l[1])):
+        if (not re.fullmatch(regex, l[1])):
             raise FormulaError("Error: Enter either + or - in the middle!")
 
-        if(l[1] == "+"):
-            print("Result: ", a+b )
-        if (l[1] == "-"):
-            print("Result: ", a-b)
-
+        calculate(a, operator, b)
     except FormulaError as e:
         print(e.msg)
 
-while(True):
-    l = list(input('\nEnter Formula (Type "quit" to exit): ').split())
-    if(l[0] == "quit"):
-        exit()
-    Calculate(l)
+def calculate(a, operator, b):
+    if operator == '+':
+        print(a + b)
+    elif operator == '-':
+        print(a - b)
 
+
+while True:
+    print(' The format is [integer - operator -integer]')
+    inputx = input("$ ")
+    if inputx == 'quit':
+        break
+    test(inputx)
